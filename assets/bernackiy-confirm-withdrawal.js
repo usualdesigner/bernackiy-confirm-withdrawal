13b0503b0932a25da95979f22e9a0290fc891d74
usualdesigner <usualdesigner@gmail.com>
2015-02-24 18:55:55 +0200

    countdown

--- assets/bernackiy-confirm-withdrawal.js
+++ assets/bernackiy-confirm-withdrawal.js
@@ -1,11 +1,10 @@
-(function () {
-    jQuery(document).ready(function ($) {
-        var unacceptable_watching_time = 60 * 1000;
-        $.cookie('bernackiy-confirm-withdrawal', Date.now());
-        window.onbeforeunload = function (e) {
-            console.log(e.target);
-            if ((parseInt($.cookie('bernackiy-confirm-withdrawal')) + parseInt(unacceptable_watching_time)) > parseInt(Date.now()))
-                return confirm("Do you really want to close?");
-        };
-    });
-}());
+//(function () {
+//    jQuery(document).ready(function ($) {
+//        var unacceptable_watching_time = 60 * 1000;
+//        $.cookie('bernackiy-confirm-withdrawal', Date.now());
+//        window.onbeforeunload = function (e) {
+//            if ((parseInt($.cookie('bernackiy-confirm-withdrawal')) + parseInt(unacceptable_watching_time)) > parseInt(Date.now()))
+//                return confirm("Do you really want to close?");
+//        };
+//    });
+//}());
--- bernackiy-confirm-withdrawal.php
+++ bernackiy-confirm-withdrawal.php
@@ -11,6 +11,8 @@ function my_wp_enqueue_scripts()
     wp_enqueue_script('jquery');
     wp_enqueue_script('jquery_cookie', plugins_url('assets/jquery-cookie/src/jquery.cookie.js', __FILE__));
     wp_enqueue_script('bernackiy_confirm_withdrawal_js', plugins_url('assets/bernackiy-confirm-withdrawal.js', __FILE__));
+
+    wp_enqueue_style('bernackiy_confirm_withdrawal_css', plugins_url('assets/bernackiy-confirm-withdrawal.css', __FILE__));
 }
 
 add_action('wp_enqueue_scripts', 'my_wp_enqueue_scripts');
